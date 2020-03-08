81dfa6fb4c042950e2d586b657df3a1deb0f361d
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-08 15:15:37 +0300

    homework04 : add exception if url is nil

--- lesson04/homework/lib/Readers/reader.rb
+++ lesson04/homework/lib/Readers/reader.rb
@@ -3,6 +3,8 @@
 # Global class reader
 module Reader
   def self.read(input)
+    raise 'Uri is empty' if input.nil?
+
     if URI::DEFAULT_PARSER.make_regexp(input)
       LinkReader.read(input)
     elsif File.file?(input)
