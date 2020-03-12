fec09aec50bf282510a8619262b2fd02fc1a0d16
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-12 11:47:53 +0300

    homework04 : fix regex for url links

--- lesson04/homework/lib/Readers/reader.rb
+++ lesson04/homework/lib/Readers/reader.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
+require 'open-uri'
 # Global class reader
 module Reader
   def self.read(input)
     raise 'Uri is empty' if input.nil?
 
-    if URI::DEFAULT_PARSER.make_regexp(input)
-      LinkReader.read(input)
-    elsif File.file?(input)
+    if File.file?(input)
       FileReader.read(input)
+    elsif input.slice(URI.regexp(%w[http https]))
+      LinkReader.read(input)
     end
   end
 end
