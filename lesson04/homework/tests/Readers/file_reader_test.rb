a92c7ffee87cc349308b50c26c8e68e863323e75
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-13 16:00:30 +0300

    homework04 : fix file path in file_reader_test

--- lesson04/homework/tests/Readers/file_reader_test.rb
+++ lesson04/homework/tests/Readers/file_reader_test.rb
@@ -6,20 +6,21 @@ require 'open-uri'
 class FileReaderTest < Minitest::Test
   def setup
     @file_reader = FileReader
+    @file_path = Dir.pwd + '/tests/Readers/test_files/'
   end
 
   # need fix hardcode path
   def test_file1
-    assert_equal 'test file with extension', @file_reader.read(Dir.pwd + '/tests/Readers/test_files/file1.txt')
+    assert_equal 'test file with extension', @file_reader.read(@file_path + 'file1.txt')
   end
 
   # need fix hardcode path
   def test_file2
-    assert_equal 'test file without extension', @file_reader.read(Dir.pwd + '/tests/Readers/test_files/file2')
+    assert_equal 'test file without extension', @file_reader.read(@file_path + 'file2')
   end
 
   # need fix hardcode path
   def test_empty_file
-    assert_equal '', @file_reader.read(Dir.pwd + '/tests/Readers/test_files/empty_file.txt')
+    assert_equal '', @file_reader.read(@file_path + 'empty_file.txt')
   end
 end
