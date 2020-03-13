460ecb3048d685debc1f29aed623262256679dbe
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-13 15:56:26 +0300

    homework04 : move module variablies to method variablies

--- lesson04/homework/main.rb
+++ lesson04/homework/main.rb
@@ -8,19 +8,18 @@ require_all('tests')
 module Main
   READERS = ['LinkReader', 'FileReader'].freeze
   PARSERS = ['AtomParser', 'RssParser'].freeze
-  @parser = ''
-  @reader = ''
-
   def self.run(options = {})
     input_data = options['input']
+    parser = ''
+    reader = ''
     Main::READERS.map do |reader_name|
-      @reader = reader_name if Module.const_get(reader_name).can_read?(input_data)
+      reader = reader_name if Module.const_get(reader_name).can_read?(input_data)
     end
-    input_string = Module.const_get(@reader).read(input_data)
+    input_string = Module.const_get(reader).read(input_data)
     Main::PARSERS.map do |parser_name|
-      @parser = parser_name if Module.const_get(parser_name).can_parse?(input_string)
+      parser = parser_name if Module.const_get(parser_name).can_parse?(input_string)
     end
-    parsed_string = Module.const_get(@parser).parse(input_string)
+    parsed_string = Module.const_get(parser).parse(input_string)
     parsed_string
   end
 end
