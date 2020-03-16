6f8e97f5fd3cdb63530431bea542599f1efd58f2
Irina Ablyazova <ulinvest73@mail.ru>
2020-03-16 14:22:53 +0300

    add rss converter

---
+++ converter/lib/converters/rss_converter.rb
@@ -0,0 +1,20 @@
+module RssConverter
+    def self.convert(data)
+      result = ''
+      data.each do |item|
+        result += "<item>\n"
+        result += "<guid> #{item[:guid]} </guid>\n"
+        result += "<title> #{item[:title]} </title>\n"
+        result += "<links> #{item[:links]} </links>\n"
+        result += "<description> #{item[:description]} </description>\n"
+        result += "<pubDate> #{item[:pubDate]} </pubDate>\n"
+        result += "<enclosure> #{item[:enclosure]} </enclosure>\n"
+        result += "<category> #{item[:category]} </category>\n"
+        result += "</item>\n"
+      end
+        result += '</rss>'
+      File.open('../file', 'w') do |file|
+        file.puts result_xml
+      end
+    end
+  end
