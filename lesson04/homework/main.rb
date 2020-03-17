9ed315b3ca0e702694e789349d80db01e9f92fca
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-17 23:09:15 +0300

    homework04 : add new functionality with writer and sorters main.rb

--- lesson04/homework/main.rb
+++ lesson04/homework/main.rb
@@ -6,20 +6,28 @@ require_all('tests')
 
 # class converter
 module Main
-  READERS = ['LinkReader', 'FileReader'].freeze
-  PARSERS = ['AtomParser', 'RssParser'].freeze
+  READERS = %w[LinkReader FileReader].freeze
+  PARSERS = %w[AtomParser RssParser JsonParser].freeze
   def self.run(options = {})
+    raise 'This args are required' if options['input'].nil? || options['output'].nil?
+
     input_data = options['input']
-    parser = ''
     reader = ''
     Main::READERS.map do |reader_name|
       reader = reader_name if Module.const_get(reader_name).can_read?(input_data)
     end
     input_string = Module.const_get(reader).read(input_data)
+    parser = ''
     Main::PARSERS.map do |parser_name|
       parser = parser_name if Module.const_get(parser_name).can_parse?(input_string)
     end
     parsed_string = Module.const_get(parser).parse(input_string)
-    parsed_string
+    unless options['sort'].nil?
+      sort_module = options['sort'].capitalize + 'Sorter'
+      Module.const_get(sort_module).sort(parsed_string[:content])
+    end
+    convert = options['output'].capitalize + 'Converter'
+    formatted_string = Module.const_get(convert).convert(parsed_string)
+    FileWriter.write(formatted_string, options['output'])
   end
 end
