149cf5a2e8576c53595a5b417f81232b6dee1ddc
Artem Trepalin <atrepalin@simtechdev.com>
2020-03-19 15:41:05 +0300

    homework04 : add new tests for rss and atom parser

--- lesson04/homework/tests/Parsers/atom_parser_test.rb
+++ lesson04/homework/tests/Parsers/atom_parser_test.rb
@@ -10,18 +10,55 @@ class AtomParserTest < Minitest::Test
 
   def test_atom_format
     hash = {
-      'title' => 'Example Feed',
-      'link' => { '@href' => 'http://example.org/' },
-      'author' => [{ 'name' => 'John Doe' }]
+      head: {
+        id: 'http://rss.dw.com/atom/rss-ru-news',
+        title: 'Example Feed 11111',
+        updated: 'today',
+        author: {
+          name: 'John Doe',
+          email: 'test@gmail.com'
+        }
+      },
+      content: [
+        {
+          title: 'Example Feed',
+          date: 'Thu, 12 Mar 2022 13:06:44 +0300'
+        }
+      ]
     }
 
     string = '<feed xmlns="http://www.w3.org/2005/Atom">'\
-                '<title>Example Feed</title>'\
-                '<link href="http://example.org/"/>'\
+                '<title>Example Feed 11111</title>'\
+                '<id>http://rss.dw.com/atom/rss-ru-news</id>'\
+                '<updated>today</updated>'\
                 '<author>'\
                   '<name>John Doe</name>'\
+                  '<email>test@gmail.com</email>'\
                 '</author>'\
+                '<entry>'\
+                  '<title>Example Feed</title>'\
+                  '<updated>Thu, 12 Mar 2022 13:06:44 +0300</updated>'\
+                '</entry>'\
               '</feed>'
+
+    assert_equal hash, @atom_parser.parse(string)
+  end
+
+  def test_empty_hash
+    hash = {
+      head: {
+      },
+      content: [
+        {
+        }
+      ]
+    }
+
+    string = '<feed xmlns="http://www.w3.org/2005/Atom">'\
+                '<entry>'\
+                '</entry>'\
+              '</feed>'
+
     assert_equal hash, @atom_parser.parse(string)
   end
 end
